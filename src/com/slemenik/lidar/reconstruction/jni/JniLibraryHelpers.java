062e990113cf86ebad00f78af1896c286ac93aa2
slemenik <ms4388@student.uni-lj.si>
2019-02-06 15:04:53 +0100

    Add sample reading points

---
+++ src/com/slemenik/lidar/reconstruction/jni/JniLibraryHelpers.java
@@ -0,0 +1,15 @@
+package com.slemenik.lidar.reconstruction.jni;
+
+public class JniLibraryHelpers {
+
+//    static {
+//        System.loadLibrary("TODOMS");
+//    }
+//
+//    public test() {
+//
+//    }
+//
+//    public static native writePoints(int x, int y, int z);
+
+}
